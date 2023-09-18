c59181cba88cc3edd1cc506860e6a539289ed552
amir <amir@dragonchain.com>
2023-09-18 12:47:06 +0100

    add loading spinner

--- src/routes/[lang]/+layout.svelte
+++ src/routes/[lang]/+layout.svelte
@@ -2,13 +2,38 @@
 	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
 	import Layout from '$lib/components/Layout.svelte';
+	import { Jumper } from 'svelte-loading-spinners';
 
 	import '../../style.css';
 	import '../../normalize.css';
+	import { onMount } from 'svelte';
+	import { browser } from '$app/environment';
+	let loading = true;
+
+	function fire() {
+		loading = false;
+	}
+	onMount(() => {
+		window.onload = fire;
+	});
 </script>
 
-<Header />
+{#if loading}
+	<div class="loading">
+		<Jumper color="red" />
+	</div>
+{:else}
+	<Header />
+	<slot />
+	<Footer />
+{/if}
 
-<slot />
-
-<Footer />
+<style>
+	.loading {
+		height: 100vh;
+		width: 100vw;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
