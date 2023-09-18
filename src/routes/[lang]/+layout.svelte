606dee1e4c63778dc3659536f5f4ea13b98f08db
amir <amir@dragonchain.com>
2023-09-18 12:50:30 +0100

    reove loading spinner

--- src/routes/[lang]/+layout.svelte
+++ src/routes/[lang]/+layout.svelte
@@ -2,38 +2,15 @@
 	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
 	import Layout from '$lib/components/Layout.svelte';
-	import { Jumper } from 'svelte-loading-spinners';
 
 	import '../../style.css';
 	import '../../normalize.css';
 	import { onMount } from 'svelte';
 	import { browser } from '$app/environment';
-	let loading = true;
-
-	function fire() {
-		loading = false;
-	}
-	onMount(() => {
-		window.onload = fire;
-	});
 </script>
 
-{#if loading}
-	<div class="loading">
-		<Jumper color="red" />
-	</div>
-{:else}
-	<Header />
-	<slot />
-	<Footer />
-{/if}
+<Header />
 
-<style>
-	.loading {
-		height: 100vh;
-		width: 100vw;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
-</style>
+<slot />
+
+<Footer />
